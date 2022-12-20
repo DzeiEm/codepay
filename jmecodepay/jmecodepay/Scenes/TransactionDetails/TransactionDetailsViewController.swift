b3d931f2684ad7b03f0267728dbaf1507873a03e
DzeiEm <julija.melesina@gmail.com>
2022-12-20 19:51:21 +0200

    details screen

--- jmecodepay/jmecodepay/Scenes/TransactionDetails/TransactionDetailsViewController.swift
+++ jmecodepay/jmecodepay/Scenes/TransactionDetails/TransactionDetailsViewController.swift
@@ -6,7 +6,18 @@ import UIKit
 
 class TransactionDetailsViewController: UIViewController {
     
+    @IBOutlet weak var receiverLabel: UILabel!
+    @IBOutlet weak var subjectLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
     
     
+    @IBAction func backButtonTapped() {
+        self.dismiss(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        //
+    }
     
 }
